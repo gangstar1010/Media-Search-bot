55c501cb5213e2ff2f42ed1c1611afdf835de4ef
gangstar1010 <88017311+gangstar1010@users.noreply.github.com>
2021-10-01 15:14:54 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -100,7 +100,7 @@ async def start(bot, cmd):
                 [
                     [
                         InlineKeyboardButton("Search Here", switch_inline_query_current_chat=''),
-                        InlineKeyboardButton("Other Bots", url="https://t.me/SH_Telugu_All_Movies")
+                        InlineKeyboardButton("More Bots", url="https://t.me/SH_Telugu_All_Movies")
                     ],
                     [
                         InlineKeyboardButton("About", callback_data="about")
